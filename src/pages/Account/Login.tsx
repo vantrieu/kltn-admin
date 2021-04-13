e66275d6e662ec0d59661760f142c8995cedcc72
vantrieu <nguyenvantrieu.it@gmail.com>
2021-04-13 15:36:16 +0700

    handle login

--- src/pages/Account/Login.tsx
+++ src/pages/Account/Login.tsx
@@ -1,51 +1,115 @@
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { AppState } from '../../store';
+import { useLocation } from 'react-router';
+import { login, logout } from '../../store/Account/actions';
+
 export const Login = () => {
+    const [inputs, setInputs] = useState({
+        email: '',
+        password: '',
+    });
+    const [submitted, setSubmitted] = useState(false);
+
+    const loading = useSelector<AppState>((state) => state.account.loading);
+
+    const { email, password } = inputs;
+
+    const dispatch = useDispatch();
+    const location = useLocation();
+
+    useEffect(() => {
+        dispatch(logout());
+    }, [dispatch]);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setInputs((inputs) => ({ ...inputs, [name]: value }));
+    };
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setSubmitted(true);
+        if (email && password) {
+            const { from }: any = location.state || { from: { pathname: '/' } };
+            dispatch(login(email, password, from));
+        }
+    };
+
     return (
-        <div className="container">
+        <div className='container'>
             {/* Outer Row */}
-            <div className="row justify-content-center">
-                <div className="col-xl-10 col-lg-12 col-md-9">
-                    <div className="card o-hidden border-0 shadow-lg my-5">
-                        <div className="card-body p-0">
+            <div className='row justify-content-center'>
+                <div className='col-xl-10 col-lg-12 col-md-9'>
+                    <div className='card o-hidden border-0 shadow-lg my-5'>
+                        <div className='card-body p-0'>
                             {/* Nested Row within Card Body */}
-                            <div className="row">
-                                <div className="col-lg-6 d-none d-lg-block bg-login-image" />
-                                <div className="col-lg-6">
-                                    <div className="p-5">
-                                        <div className="text-center">
-                                            <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                            <div className='row'>
+                                <div className='col-lg-6 d-none d-lg-block bg-login-image' />
+                                <div className='col-lg-6'>
+                                    <div className='p-5'>
+                                        <div className='text-center'>
+                                            <h1 className='h4 text-gray-900 mb-4'>Welcome Back!</h1>
                                         </div>
-                                        <form className="user">
-                                            <div className="form-group">
-                                                <input type="email" className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
+                                        <form className='user' onSubmit={handleSubmit}>
+                                            <div className='form-group'>
+                                                <input
+                                                    type='email'
+                                                    className={
+                                                        'form-control form-control-user ' +
+                                                        (submitted && !email ? 'is-invalid' : '')
+                                                    }
+                                                    id='exampleInputEmail'
+                                                    aria-describedby='emailHelp'
+                                                    onChange={handleChange}
+                                                    placeholder='Enter Email Address...'
+                                                    name='email'
+                                                />
+                                                {submitted && !email && (
+                                                    <div className='invalid-feedback' style={{display: "flex", justifyContent: "center"}}>
+                                                        Email is required
+                                                    </div>
+                                                )}
                                             </div>
-                                            <div className="form-group">
-                                                <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
+                                            <div className='form-group'>
+                                                <input
+                                                    type='password'
+                                                    className={
+                                                        'form-control form-control-user ' +
+                                                        (submitted && !email ? 'is-invalid' : '')
+                                                    }
+                                                    id='exampleInputPassword'
+                                                    placeholder='Password'
+                                                    onChange={handleChange}
+                                                    name='password'
+                                                    autoComplete="on"
+                                                />
+                                                {submitted && !password && (
+                                                    <div className='invalid-feedback' style={{display: "flex", justifyContent: "center"}}>
+                                                        Password is required
+                                                    </div>
+                                                )}
                                             </div>
-                                            <div className="form-group">
-                                                <div className="custom-control custom-checkbox small">
-                                                    <input type="checkbox" className="custom-control-input" id="customCheck" />
-                                                    <label className="custom-control-label" htmlFor="customCheck">Remember
-                                                    Me
-                                                </label>
-                                                </div>
+                                            <div className='form-group' style={{display: "flex", justifyContent: "center"}}>
+                                                <button className='btn btn-primary'>
+                                                    {loading && (
+                                                        <span className='spinner-border spinner-border-sm mr-1'></span>
+                                                    )}
+                          Login
+                        </button>
                                             </div>
-                                            <a href="index.html" className="btn btn-primary btn-user btn-block">
-                                                Login
-                                            </a>
-                                            <hr />
-                                            <a href="index.html" className="btn btn-google btn-user btn-block">
-                                                <i className="fab fa-google fa-fw" /> Login with Google
-                                            </a>
-                                            <a href="index.html" className="btn btn-facebook btn-user btn-block">
-                                                <i className="fab fa-facebook-f fa-fw" /> Login with Facebook
-                                            </a>
                                         </form>
                                         <hr />
-                                        <div className="text-center">
-                                            <a className="small" href="forgot-password.html">Forgot Password?</a>
+                                        <div className='text-center'>
+                                            <a className='small' href='forgot-password.html'>
+                                                Forgot Password?
+                      </a>
                                         </div>
-                                        <div className="text-center">
-                                            <a className="small" href="register.html">Create an Account!</a>
+                                        <div className='text-center'>
+                                            <a className='small' href='register.html'>
+                                                Create an Account!
+                      </a>
                                         </div>
                                     </div>
                                 </div>
@@ -55,5 +119,5 @@ export const Login = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
---
+++ src/store/Account/actions.ts
@@ -0,0 +1,33 @@
+import { userService } from './../../services/user.service';
+import { AccountActionTypes, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOG_OUT } from './types';
+import { Dispatch } from "react";
+import { history } from '../../helpers';
+
+export const login = (email: string, password: string, from: string) => {
+    return (dispatch: Dispatch<AccountActionTypes>) => {
+        dispatch({
+            type: LOGIN_REQUEST,
+            payload: {
+                email: email,
+                password: password
+            }
+        });
+
+        userService.login(email, password).then((res) => {
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res
+            });
+            history.push(from);
+        }, (error) => {
+            dispatch({
+                type: LOGIN_FAILURE,
+                payload: {error: error.toString()}
+            });
+        }); 
+    }
+}
+
+export const logout = (): AccountActionTypes => {
+    return {type: LOG_OUT}
+}
