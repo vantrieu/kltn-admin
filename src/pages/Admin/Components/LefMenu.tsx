685e4e51334c59303f88bb3d483dcfb1e9732789
vantrieu <nguyenvantrieu.it@gmail.com>
2021-04-21 09:41:08 +0700

    Refacter Components

---
+++ src/pages/Admin/Components/LefMenu.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const LefMenu = () => {
+    const [toggled, setToggled] = useState(false);
+
+    return (
+        <ul className={"navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" + (toggled ? ' toggled' : '')} id="accordionSidebar">
+            <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/">
+                <div className="sidebar-brand-icon rotate-n-15">
+                    <i className="fas fa-laugh-wink" />
+                </div>
+                <div className="sidebar-brand-text mx-3">HEAVEN SHOP</div>
+            </Link>
+            <hr className="sidebar-divider" />
+            <div className="sidebar-heading">
+                Quản trị hệ thống
+            </div>
+            <li className="nav-item">
+                <Link className="nav-link" to='/users'>
+                    <i className="fas fa-users" />
+                    <span>Người dùng</span>
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to='/products'>
+                    <i className="fas fa-fw fa-table" />
+                    <span>Sản phẩm</span>
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to='/categories'>
+                    <i className="fab fa-typo3" />
+                    <span>Loại sản phẩm</span>
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to='/orders'>
+                    <i className="fab fa-jedi-order" />
+                    <span>Đơn hàng</span>
+                </Link>
+            </li>
+            <hr className="sidebar-divider d-none d-md-block" />
+            <div className="text-center d-none d-md-inline" onClick={() => setToggled(!toggled)}>
+                <button className="rounded-circle border-0" id="sidebarToggle" />
+            </div>
+        </ul>
+    )
+}
+
+
+export default LefMenu;
---
+++ src/pages/Admin/Components/TopNavbar.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../../store/Account/actions";
+
+const TopNavbar = () => {
+    const [isShowProfilemenuDropdown, setIsShowProfilemenuDropdown] = useState(false);
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <ul className="navbar-nav ml-auto">
+                    {/* Nav Item - User Information */}
+                    <li className={"nav-item dropdown no-arrow" + (isShowProfilemenuDropdown ? ' show' : '')}>
+                        <label className="nav-link dropdown-toggle" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded={isShowProfilemenuDropdown ? 'true' : 'false'} onClick={() => setIsShowProfilemenuDropdown(!isShowProfilemenuDropdown)}>
+                            <span className="mr-2 d-none d-lg-inline text-gray-600 small">Supper Admin</span>
+                            <img className="img-profile rounded-circle" src="/undraw_profile.svg" alt="undraw_profile" />
+                        </label>
+                        {/* Dropdown - User Information */}
+                        <div className={"dropdown-menu dropdown-menu-right shadow animated--grow-in" + (isShowProfilemenuDropdown ? ' show' : '')} aria-labelledby="userDropdown">
+                            <label className="dropdown-item" data-toggle="modal" data-target="#logoutModal" onClick={() => dispatch(logout())}>
+                                <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" />
+                                Logout
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default TopNavbar;
